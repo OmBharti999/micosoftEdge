6e4fdcd8a0b08817016469a583813a301e29b0a1
OmBharti999 <55656898+OmBharti999@users.noreply.github.com>
2022-10-10 23:32:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Routes from "./Routes";
+import Nav from "./components/Nav/Nav";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Routes />
+      <Nav />
+    </div>
+  );
+}
---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
+// components
+// import Routes from "./Routes";
+
+export default function R() {
+  return (
+    <Routes>
+      <Route path="/home" />
+    </Routes>
+  );
+}
