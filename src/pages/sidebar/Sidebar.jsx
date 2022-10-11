f79704267cfe88bfe7aed690f0ea23e31b28b490
Om <ombharti888@gmail.com>
2022-10-11 13:09:48 +0000

    Update 4 files

--- src/pages/sidebar/Sidebar.jsx
+++ src/pages/sidebar/Sidebar.jsx
@@ -1,10 +1,26 @@
+import ImgSide from "./img/sidebar.png";
+import Ct from "../../components/coloredText/ColoredText";
+import But from "../../components/button/But"
+
 import "./si.css";
+
 export default function Sidebar() {
   return (
     <div className="sidebar-conataier">
-      <div>Microsoft Edge has been updated</div>
-      <div>
-        <img scr="" />
+      <div className="sidebar-first">
+        <h1>
+          {" "}
+          <Ct text="Multitasking" /> with your sidebar{" "}
+        </h1>
+        <div>
+          Get more done on the web with tools, apps, and more just a click away.
+          Say goodbye to switching tabs.
+        </div>
+
+        <But text="Next" />
+      </div>
+      <div className="sidebar-sec">
+        <img className="sidebar-sec" scr={ImgSide} alt="side" />
       </div>
     </div>
   );
