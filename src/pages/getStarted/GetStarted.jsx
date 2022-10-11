ddf66667808eea73d65848165ed906fc91c2af89
Om <ombharti888@gmail.com>
2022-10-11 08:58:34 +0000

    Update 3 files

--- src/pages/getStarted/GetStarted.jsx
+++ src/pages/getStarted/GetStarted.jsx
@@ -1,6 +1,14 @@
+import Card from "../../components/card/Card"
+
+import Img1 from "./img/micro1st.png";
+import Img2 from "./img/micro2nd.png";
+import Img3 from "./img/micro3rd.png";
+
 import "./getStarted.css";
 export default function GetStarted() {
   return (
-    <>You're three steps away from getting the best browsing experience.</>
+    <><h2>You're three steps away from getting the best browsing experience.
+    </h2>
+    </>
   );
 }
