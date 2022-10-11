e991c08a6722ed3fff58ca75a8daf456bf8df0fc
Om <ombharti888@gmail.com>
2022-10-11 08:40:51 +0000

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
+import GetStarted from "./pages/getStarted/GetStarted";
 
 // components
 // import Routes from "./Routes";
@@ -8,6 +9,7 @@ export default function R() {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
+      <Route path="/getStarted" element={<GetStarted />} />
     </Routes>
   );
 }
