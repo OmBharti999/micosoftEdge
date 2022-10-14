12fd4ba7740be46a1e95973661e661503bd4a60b
Om <ombharti888@gmail.com>
2022-10-14 16:41:59 +0000

    Update 5 files

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -39,18 +39,18 @@ function BrandExample() {
                 <Nav.Link href="#feeds">News feed</Nav.Link>
               </Link>
               <Link className="nav-bar anchor" to="/Shopping">
-                <Nav.Link href="#Shopping">Shopping in sidebar</Nav.Link>
+                <Nav.Link href="/Shopping">Shopping in sidebar</Nav.Link>
               </Link>
               <Link className="nav-bar anchor" to="/activity">
-                <Nav.Link href="#Shoppingactivity">Shopping activity</Nav.Link>
+                <Nav.Link href="/activity">Shopping activity</Nav.Link>
               </Link>
               {/* "/sidebar" */}
               <Link className="nav-bar anchor" to="/sidebar">
                 {" "}
-                <Nav.Link href="#sidebar">Sidebar</Nav.Link>
+                <Nav.Link href="/sidebar">Sidebar</Nav.Link>
               </Link>{" "}
               <Link className="nav-bar anchor" to="/getStarted">
-                <Nav.Link eventKey={2} href="#getStarted">
+                <Nav.Link eventKey={2} href="/getStarted">
                   Get started
                 </Nav.Link>
               </Link>
--- src/components/card2/Card2.jsx
+++ src/components/card2/Card2.jsx
@@ -1,4 +1,20 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
 import "./card2.css";
-export default function Card2() {
-    return <>Microsoft Edge has been updated</>
+
+export default function Card2({ imgsrc, title, content, url }) {
+  return (
+    <Card style={{ width: "16rem" }}>
+      <Card.Img variant="top" src={imgsrc} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{content} </Card.Text>
+        <a href={url}>
+          {" "}
+          <Button variant="primary">Go somewhere</Button>
+        </a>
+      </Card.Body>
+    </Card>
+  );
 }
